fc75a9aabae77005469a23aebf1a4f94bb15feee
coderdivya18 <coderdivya18@gmail.com>
2025-05-05 16:49:23 +0530

    Font family updated with final touch and change theme features

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector('[data-navbar]');
 const navbarTogglerElements = document.querySelectorAll('[data-nav-toggler]');
 const overlay = document.querySelector('[data-overlay]');
+const themeBtn=document.querySelector('#theme-btn');
+const body=document.querySelector('body');
+const getAllDividers=document.querySelectorAll('.divider');
 
 
 function handleElementsOnClick(elements,eventType, callback) {
@@ -16,3 +19,30 @@ function handleNavbarTogglerClick() {
 }
 
 handleElementsOnClick(navbarTogglerElements, 'click', handleNavbarTogglerClick);
+
+themeBtn.addEventListener('click',()=>{
+    if(body.classList.contains('black-theme')){
+        body.classList.remove('black-theme');
+        body.classList.add('blue-theme');
+
+        getAllDividers.forEach(item=>{
+            item.setAttribute('data-theme','blue')
+        })
+
+    }else if(body.classList.contains('blue-theme')){
+        body.classList.remove('blue-theme');
+        body.classList.add('violet-theme');
+
+        getAllDividers.forEach(item=>{
+            item.setAttribute('data-theme','violet')
+        })
+
+    }else if(body.classList.contains('violet-theme')){
+        body.classList.remove('violet-theme');
+        body.classList.add('black-theme');
+
+        getAllDividers.forEach(item=>{
+            item.setAttribute('data-theme','black')
+        })
+    }
+})
